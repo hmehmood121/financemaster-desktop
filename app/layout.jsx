94ceede1051c4b7247c98d4239e26bd1f47a81f6
Hasan Mehmood <hmehmood121@gmail.com>
2025-03-03 22:49:56 +0500

    toast top-center

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
-        <Toaster />
+        <Toaster position="top-center" />
       </body>
     </html>
   )
